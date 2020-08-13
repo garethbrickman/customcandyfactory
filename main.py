b519bd753f99d3ba6aeade5a0dee1b7d0b6caeb6
william <191marquis@gmail.com>
2020-08-13 14:37:02 -0400

    [INIT] Create basic api structured

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from typing import Optional
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/orders")
 def read_root():
+""" get all orders (redacted if no auth) """
     return {"Hello": "World"}
 
+@app.post("/orders")
+def read_root():
+""" make a new order """
+    return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/orders/{order_id}")
+def read_item(order_id):
+""" get an order (redacted if no auth)"""
+    return {"order_id": order_id}
+
+@app.put("/orders/{order_id}")
+def read_root(order_id):
+""" change an order """
+    return {"Hello": "World"}
+
