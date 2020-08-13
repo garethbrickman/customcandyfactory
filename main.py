66da144a0c2da01a714f2c6ad25106948322b899
william <191marquis@gmail.com>
2020-08-13 16:00:20 -0400

    [DEV] Restructure

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ def read_root():
     """ get all orders (redacted if no auth) """
     return {"Hello": "World"}
 
+
 @app.post("/orders")
 def read_root():
     """ make a new order """
     return {"Hello": "World"}
 
+
 @app.get("/orders/{order_id}")
 def read_item(order_id):
     """ get an order (redacted if no auth)"""
     return {"order_id": order_id}
+
 
 @app.put("/orders/{order_id}")
 def read_root(order_id):
