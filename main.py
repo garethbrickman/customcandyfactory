c6417923bf306c58bab035b2f67e93a0a7f4268d
william <191marquis@gmail.com>
2020-08-13 16:02:52 -0400

    [DEV] Add temporary endpoints in lieu of auth

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = FastAPI()
 def read_root():
     """ get all orders (redacted if no auth) """
     return {"Hello": "World"}
+@app.get("/orders/candyman")
+def read_root():
+    """ get all orders (redacted if no auth) """
+    return {"Auth": "Candyman"}
 
 
 @app.post("/orders")
@@ -19,6 +23,10 @@ def read_root():
 def read_item(order_id):
     """ get an order (redacted if no auth)"""
     return {"order_id": order_id}
+@app.get("/orders/{order_id}/candyman")
+def read_item(order_id):
+    """ get an order (redacted if no auth)"""
+    return {"order_id": order_id, "auth": "candyman"}
 
 
 @app.put("/orders/{order_id}")
