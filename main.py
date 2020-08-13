a55a30530b06f1b8b27ca10d0e5c077de5231454
william <191marquis@gmail.com>
2020-08-13 14:41:59 -0400

    [BUGFIX] Indent docstrings

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ app = FastAPI()
 
 @app.get("/orders")
 def read_root():
-""" get all orders (redacted if no auth) """
+    """ get all orders (redacted if no auth) """
     return {"Hello": "World"}
 
 @app.post("/orders")
 def read_root():
-""" make a new order """
+    """ make a new order """
     return {"Hello": "World"}
 
 @app.get("/orders/{order_id}")
 def read_item(order_id):
-""" get an order (redacted if no auth)"""
+    """ get an order (redacted if no auth)"""
     return {"order_id": order_id}
 
 @app.put("/orders/{order_id}")
 def read_root(order_id):
-""" change an order """
+    """ change an order """
     return {"Hello": "World"}
 
